907761d964405b145b7efccde4353b7043be079d
Jorge Villada <jorge-1409@hotmail.com>
2014-04-30 23:17:45 -0500

    agregando más casos de prueba, ahora se comprueba que el resultado de una suma determinada sea válida. Se agrega documentación.

--- Test/CalculadoraTest.php
+++ Test/CalculadoraTest.php
@@ -1,27 +1,49 @@
 <?php
-class CalculadoraTest extends PHPUnit_Framework_TestCase
-{
-	private $calculadora = null;
 
-	public function setUp(){
-		$this->calculadora = new Univalle\Calculadora();
-	}
-	public function tearDown(){
-		$this->calculadora = null;
-	}
-	/**
-	* @test
-	*/
-	public function testInstanciaDe(){
-		$this->assertInstanceOf("\Univalle\Calculadora", $this->calculadora);
-	}
+/**
+ * Pruebas unitarias para la clase Univalle\Calculadora;
+ */
+class CalculadoraTest extends PHPUnit_Framework_TestCase {
 
-	/**
-	* @expectedException InvalidArgumentException
-	*/
-	public function testExceptionEsperada(){
-		$this->calculadora->suma("no_es_un_numero", 5);
-	}
+    private $calculadora = null;
+
+    /**
+     * configuración del entorno de prueba
+     */
+    public function setUp() {
+        $this->calculadora = new Univalle\Calculadora();
+    }
+
+    /**
+     * Al terminar la prueba
+     */
+    public function tearDown() {
+        $this->calculadora = null;
+    }
+
+    /**
+     * Se verifica si $this->calculadora es instancia de \Univalle\Calculadora
+     * @test
+     */
+    public function testInstanciaDe() {
+        $this->assertInstanceOf("\Univalle\Calculadora", $this->calculadora);
+    }
+
+    /**
+     * Se comprueba si el método suma arroja error cuando uno de los argumentos no es un número
+     * @expectedException InvalidArgumentException
+     */
+    public function testExceptionEsperada() {
+        $this->calculadora->suma("no_es_un_numero", 5);
+    }
+    
+    /**
+     * Comprueba si lo arrojado por el método suma, es válido
+     * @test
+     */
+    public function testResultadoValido() {
+        $this->assertEquals(5, $this->calculadora->suma(3, 2));
+    }   
 
 }
-?>
+
